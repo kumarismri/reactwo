c789e3ead15d3728b490b675e56dc80e9e350ece
Smriti Kumari <90670993+kumarismri@users.noreply.github.com>
2021-12-27 18:23:56 +0530

    reactwo

---
+++ src/Component/ComponentOne.js
@@ -0,0 +1,58 @@
+import React,{useState} from 'react'
+
+const ComponentOne = () => {
+
+    const[state,setState]=useState(
+        {
+            name:"",
+            department:"",
+            rating:""
+        }
+    )
+
+    const [secState,setSecstate]=useState([])
+
+    const eventFunction=(event)=>{
+
+        setState({...state,[event.target.name]:event.target.value})
+
+    }
+
+    const submitFun=(e)=>{
+        e.preventDefault()
+        setSecstate([...secState,{...state}])
+    }
+    return (
+        <div className='parent'>
+            <form onSubmit={submitFun}>
+                
+                <label name="name">Name:</label>
+                <input className="btn" type="text" name="name" value={state.name} onChange={eventFunction}/><br/><br/>
+                
+                
+                <label name="department">Department:</label>
+                <input className="btn" type="text" name="department" value={state.department} onChange={eventFunction}/><br/><br/>
+            
+   
+                <label name="rating">Rating:</label>
+                <input className="btn" type="number" name="rating" value={state.rating} onChange={eventFunction}/><br/><br/>
+                
+           
+                <input className="btnn" type="submit"/>               
+
+            </form>
+
+            <div className='card'>{        
+                    secState.map((value,index)=>{
+                        return(
+                            <div className="innerCard">Name:{value.name}| Department:{value.department}|Rating:{value.rating}</div>
+                        )                 
+                    })
+                }   
+            </div>
+            
+        </div>
+    )
+}
+
+export default ComponentOne
